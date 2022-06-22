763a9789e75db63dcad6042240a2b84f74597b00
Lucas <lucas-lopes22@live.com>
2022-06-22 17:02:35 -0300

    Ajusta inputs do Gerenciador

--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -1,164 +1,161 @@
 <template>
   <div class="Manager">
-      <v-card
-        class="cardContainer"
-        max-width="1200px"
-        width="100%"
-        elevation="5"
-      >
-        <v-card class="pa-5">
-          <v-row>
-            <v-col class="colunaUM" >
-              <v-row class="card-header" align="center">
-                <v-col cols="12" md="10" sm="10">
-                  <v-card-title class="text-h6"> Gerenciador </v-card-title>
+    <v-card class="cardContainer" max-width="1200px" width="100%" elevation="5">
+      <v-card class="pa-5">
+        <v-row>
+          <v-col class="colunaUM">
+            <v-row class="card-header" align="center">
+              <v-col cols="12" md="10" sm="10">
+                <v-card-title class="text-h6"> Gerenciador </v-card-title>
+              </v-col>
+              <v-col cols="2" align="center" md="2" sm="2">
+                <v-btn class="fab depressed" icon>
+                  <v-icon> mdi-plus </v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-divider class="mb-2 mt-2" />
+            <v-row>
+              <v-col>
+                <v-treeview
+                  nultiple="false"
+                  open-on-click
+                  hoverable
+                  :items="items"
+                  class="pa-0 mb-0 tree"
+                  transition
+                  aria-expanded="true"
+                >
+                  <template v-slot:label="{ item }" expand-icon="none">
+                    <div class="itemSelected" @click="bindItems(item)">
+                      <span>{{ item.name }}</span>
+                    </div>
+                  </template>
+                </v-treeview>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-divider vertical class="mx-2" />
+          <v-col class="colunaDOIS">
+            <div v-if="!selectedItem.name">
+              <v-row class="card-header-colDOIS">
+                <v-col cols="12" md="10" sm="9">
+                  <v-card-title class="text-h6">
+                    Selecione um menu ou crie um novo.
+                  </v-card-title>
                 </v-col>
-                <v-col cols="2" align="center" md="2" sm="2">
-                  <v-btn class="fab depressed" icon>
-                    <v-icon> mdi-plus </v-icon>
+              </v-row>
+            </div>
+            <div v-else>
+              <v-row class="card-header-colDOIS">
+                <v-col cols="12" md="10" sm="9">
+                  <v-card-title class="text-h6">
+                    {{ selectedItem.name }}
+                  </v-card-title>
+                </v-col>
+                <v-col cols="3" md="2" sm="3" align-self="center">
+                  <v-card-text class="pa-0" v-if="selectedItem.id"
+                    ><strong>ID: {{ selectedItem.id }}</strong>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+
+              <v-row>
+                <v-col cols="12">
+                  <v-card class="elevation-0">
+                    <v-row>
+                      <v-col>
+                        <v-row class="pa-2">
+                          <v-col>
+                            <v-text-field
+                              color="primary"
+                              :value="selectedItem.name"
+                              label="Nome"
+                            >
+                            </v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-col>
+                      <v-col>
+                        <v-row class="pa-2">
+                          <v-col>
+                            <v-text-field
+                              color="primary"
+                              :value="selectedItem.icon"
+                              label="Ícone"
+                              append-icon="mdi-alert-circle-outline"
+                              :prepend-icon="selectedItem.icon"
+                              @click:append="openIconPage"
+                            >
+                            </v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-col>
+                    </v-row>
+                    <v-row class="pa-2">
+                      <v-col>
+                        <v-text-field
+                          color="primary"
+                          :value="selectedItem.route"
+                          label="Rota"
+                        >
+                        </v-text-field>
+                      </v-col>
+                    </v-row>
+                    <v-row class="pa-2">
+                      <v-col>
+                        <v-text-field
+                          color="primary"
+                          :value="selectedItem.role"
+                          label="Permissões"
+                        >
+                        </v-text-field>
+                      </v-col>
+                    </v-row>
+
+
+                    <v-row class="pa-2">
+                      <v-col>
+                        <v-textarea
+                          color="primary"
+                          :value="selectedItem.role"
+                          label="Descrição"
+                          counter
+                        >
+                        </v-textarea>
+                      </v-col>
+                    </v-row>
+                  </v-card>
+                </v-col>
+              </v-row>
+              <v-row class="card-footer pa-3">
+                <v-col cols="12 pa-0" sm="6" md="6" lg="6" align="left">
+                  <v-btn class="footer-btn success">
+                    <span class="footer-btn-title"> Adicionar</span>
+
+                    <v-icon class="ml-2"> mdi-folder-plus </v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col cols="3 pa-0" sm="3" md="3" lg="3" align="right">
+                  <v-btn class="footer-btn warning">
+                    <span class="footer-btn-title"> Deletar</span>
+
+                    <v-icon class="ml-2"> mdi-delete </v-icon>
+                  </v-btn>
+                </v-col>
+                <v-col cols="3 pa-0" sm="3" md="3" lg="3" align="right">
+                  <v-btn class="footer-btn primary" dark>
+                    <span class="footer-btn-title"> Salvar</span>
+
+                    <v-icon class="ml-2 pa-0"> mdi-content-save </v-icon>
                   </v-btn>
                 </v-col>
               </v-row>
-              <v-divider class="mb-2 mt-2" />
-              <v-row >
-                <v-col >
-                  <v-treeview
-                    nultiple="false"
-                    open-on-click
-                    hoverable
-                    :items="items"
-                    class="pa-0 mb-0 tree"
-                    transition
-                    aria-expanded="true"
-                    
-                  >
-                    <template v-slot:label="{ item }" expand-icon="none">
-                      <div class="itemSelected" @click="bindItems(item)">
-                        <span>{{ item.name }}</span>
-                      </div>
-                    </template>
-                  </v-treeview>
-                </v-col>
-              </v-row>
-            </v-col>
-            <v-divider vertical class="mx-2" />
-            <v-col class="colunaDOIS">
-              <div v-if="!selectedItem.name">
-                <v-row class="card-header-colDOIS">
-                  <v-col cols="12" md="10" sm="9">
-                    <v-card-title class="text-h6">
-                      Selecione um menu ou crie um novo.
-                    </v-card-title>
-                  </v-col>
-                </v-row>
-              </div>
-              <div v-else>
-                <v-row class="card-header-colDOIS">
-                  <v-col cols="12" md="10" sm="9">
-                    <v-card-title class="text-h6">
-                      {{ selectedItem.name }}
-                    </v-card-title>
-                  </v-col>
-                  <v-col cols="3" md="2" sm="3" align-self="center">
-                    <v-card-text class="pa-0" v-if="selectedItem.id"
-                      ><strong>ID: {{ selectedItem.id }}</strong>
-                    </v-card-text>
-                  </v-col>
-                </v-row>
-
-                <v-row>
-                  <v-col cols="12">
-                    <v-card class="elevation-0">
-                      <v-row class="pa-2">
-                        <v-col>
-                          <v-text-field
-                            color="primary"
-                            :value="selectedItem.name"
-                            label="Nome"
-                          >
-                          </v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row class="pa-2">
-                        <v-col>
-                          <v-text-field
-                            color="primary"
-                            :value="selectedItem.route"
-                            label="Rota"
-                          >
-                          </v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row class="pa-2">
-                        <v-col>
-                          <v-text-field
-                            color="primary"
-                            :value="selectedItem.role"
-                            label="Permissões"
-                          >
-                          </v-text-field>
-                        </v-col>
-                      </v-row>
-                      <v-row>
-                        <v-col>
-                          <v-row class="pa-2">
-                            <v-col>
-                              <v-text-field
-                                color="primary"
-                                :value="selectedItem.idMenu"
-                                label="Menu Pai"
-                              >
-                              </v-text-field>
-                            </v-col>
-                          </v-row>
-                        </v-col>
-                        <v-col>
-                          <v-row class="pa-2">
-                            <v-col>
-                              <v-text-field
-                                color="primary"
-                                :value="selectedItem.icon"
-                                label="Ícone"
-                                append-icon="mdi-alert-circle-outline"
-                                :prepend-icon="selectedItem.icon"
-                                @click:append="openIconPage"
-                              >
-                              </v-text-field>
-                            </v-col>
-                          </v-row>
-                        </v-col>
-                      </v-row>
-                    </v-card>
-                  </v-col>
-                </v-row>
-                <v-row class="card-footer pa-3">
-                  <v-col cols="12 pa-0" sm="6" md="6" lg="6" align="left">
-                    <v-btn class="footer-btn success">
-                      <span class="footer-btn-title"> Adicionar</span>
-
-                      <v-icon class="ml-2"> mdi-folder-plus </v-icon>
-                    </v-btn>
-                  </v-col>
-                  <v-col cols="3 pa-0" sm="3" md="3" lg="3" align="right">
-                    <v-btn class="footer-btn warning">
-                      <span class="footer-btn-title"> Deletar</span>
-
-                      <v-icon class="ml-2"> mdi-delete </v-icon>
-                    </v-btn>
-                  </v-col>
-                  <v-col cols="3 pa-0" sm="3" md="3" lg="3" align="right">
-                    <v-btn class="footer-btn primary" dark>
-                      <span class="footer-btn-title"> Salvar</span>
-
-                      <v-icon class="ml-2 pa-0"> mdi-content-save </v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </div>
-            </v-col>
-          </v-row>
-        </v-card>
+            </div>
+          </v-col>
+        </v-row>
       </v-card>
+    </v-card>
     <IconsPageVue
       v-model="dialogIcon"
       @update:value="(newValue) => (dialogIcon = newValue)"
@@ -193,7 +190,7 @@ export default {
     },
   },
   async created() {
-    this.$alert.warning("gerenciador")
+    this.$alert.warning("gerenciador");
     await this.$http("assets/menu.json").then((response) => {
       this.items = response.data;
     });
